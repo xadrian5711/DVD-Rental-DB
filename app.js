199624bd3892e13a1d53b59f11f14292668068db
xadrian5711 <xadrian5711@icloud.com>
2026-06-10 10:11:34 -0600

    gets api and adds film table

--- app.js
+++ app.js
@@ -10,13 +10,25 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static());
+app.use(express.static("public"));
+
+app.get("/api/films", async (req, res) => {
+  try {
+    const result = await pool.query(
+      `SELECT film_id, title, description. release_year, rental_rate
+            FROM film
+            ORDER BY title ASC
+            LIMIT 10`,
+    );
+    res.json(result.rows);
+  } catch (error) {}
+});
 
 app.get("/", (req, res) => {
   res.status(404).redirect("/");
 });
 
 app.listen(port, () => {
-  console.log(`DVD Store demo running on http://localhost${port}`);
+  console.log(`DVD Store demo running on http://localhost:${port}`);
   console.log(`Using Database ${DB_URL}`);
 });
