b805ccffcc63a6b9f945fb093ae6a657191e450f
xadrian5711 <xadrian5711@icloud.com>
2026-06-10 09:41:55 -0600

    adds express

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 import express from "express";
 import { Pool } from "pg";
 import path from "path";
+
+const DB_URL = process.env.DATABASE_URL || "postgresql://postgres";
+const pool = new Pool({ connectionString: DB_URL });
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static());
+
+app.get("/", (req, res) => {
+  res.status(404).redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`DVD Store demo running on http://localhost${port}`);
+  console.log(`Using Database ${DB_URL}`);
+});
