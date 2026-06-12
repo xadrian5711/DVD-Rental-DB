22f64a1431423b2982fd1a8335d8bb0407f94da3
xadrian5711 <xadrian5711@icloud.com>
2026-06-12 09:59:24 -0600

    adds updating PUT feature

--- app.js
+++ app.js
@@ -21,7 +21,33 @@ app.get("/api/films", async (req, res) => {
             LIMIT 10`,
     );
     res.json(result.rows);
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error cannot find films");
+  }
+});
+
+app.put("/api/film/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description, release_year } = req.body;
+
+  if (!title) {
+    return res.status(400).json({ error: `Title is required.` });
+  }
+  try {
+    const result = await pool.query(
+      ` UPDATE film
+            SET title = $1, description = $2, release_year = $3
+            WHERE film_id, title, description, release_year, rental_rate`,
+      [title, description || "", release_year || null, id],
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: `Filf with ID ${id} not found.:/` });
+    }
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.error(`Error updating film`, error);
+    res.status(500).json({ error: `Internal server error.` });
+  }
 });
 
 app.get("/", (req, res) => {
